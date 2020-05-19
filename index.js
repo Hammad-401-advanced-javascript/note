aeeba0504066ffbfe184382d3b40151dbe646250
Hammadsoloman <hammadali95@yahoo.com>
2020-05-20 01:33:31 +0300

    some edit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
+
+// from the export
+
 const Input = require('./lib/input');
 const Notes = require('./lib/notes');
 
+// connect with note
 const newResult = new Input();
 const note = new Notes(newResult);
 
