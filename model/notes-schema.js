f2abfd7105d2000025ea08d0bc787e0bf65f1b56
Hammadsoloman <hammadali95@yahoo.com>
2020-05-20 04:32:49 +0300

    comment library req

---
+++ model/notes-schema.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// new library required
+const mongoose = require('mongoose');
+
+const notes = mongoose.Schema({
+  text: { type: String, required: true },
+  category: { type: String, required: true },
+});
+
+
+module.exports = mongoose.model('notes', notes);
